c79a1fb90423f750888ca7d048dd40b6bce5b5b9
Myrenic <mike@tuntelder.com>
2025-03-28 19:08:42 +0100

    rollback dev changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    DATABASE_API_URL: "http://localhost:5000",
-    CISCO_API_URL: "http://localhost:5001"
+    DATABASE_API_URL: "REACT_APP_DATABASE_API_URL_PLACEHOLDER",
+    CISCO_API_URL: "REACT_APP_CISCO_API_URL_PLACEHOLDER"
   };
   
   export default config;
