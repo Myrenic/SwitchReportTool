7a461f285db166e1bfd7aaa8c6ecdc619c6dce2c
Myrenic <mike@tuntelder.com>
2025-03-31 08:41:58 +0200

    Fix for env vars in vite.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
+// src/config.js
 const config = {
-  DATABASE_API_URL: "REACT_APP_DATABASE_API_URL_PLACEHOLDER",
-  CISCO_API_URL: "REACT_APP_CISCO_API_URL_PLACEHOLDER"
+  DATABASE_API_URL: import.meta.env.VITE_REACT_APP_DATABASE_API_URL,
+  CISCO_API_URL: import.meta.env.VITE_REACT_APP_CISCO_API_URL
 };
 
 export default config;
